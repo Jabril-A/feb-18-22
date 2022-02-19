200bad4827b3ad88bb4a6dff05a470e96323fd06
Jabril-A <98670900+Jabril-A@users.noreply.github.com>
2022-02-19 13:40:00 -0600

    Add files via upload

---
+++ trooper pooper.js
@@ -0,0 +1,19 @@
+{
+    "sigleQuote"; true,
+    "arrowParens"; "avoid"
+}
+
+'use strict';
+console.log(doument.querySelector('.message'). 
+textContent);
+//video 70
+document.querySelector('message').textContent ='good job';
+
+
+document.querySelector('number').textContent ='13';
+document.querySelector('score').textContent ='10';
+
+document.querySelector('guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function() {const guess = Number(document.querySelector('.guess'))})
